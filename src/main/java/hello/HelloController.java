cc151b79c845fbfb3cba80b5d16f0331839d74e6
arockia-dew <arockia@dewsoftware.com>
2018-09-01 22:50:41 +0530

    add one commit2

--- src/main/java/hello/HelloController.java
+++ src/main/java/hello/HelloController.java
@@ -8,7 +8,7 @@ public class HelloController {
     
     @RequestMapping("/")
     public String index() {
-	System.out.println("Test");
+	System.out.println("Test123");
         return "Greetings from Spring Boot!";
     }
     
